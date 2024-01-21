4b646c0ac6d6360f6e8104032c57ba18ec8ed8c9
Yury Butskevich <bxyura@gmail.com>
2024-01-21 16:58:51 +0300

    feat: complete task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { stdout } = process;
+
+const pathStyles = path.join(__dirname, 'styles');
+const pathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function createBundle() {
+  try {
+    const files = await fs.readdir(pathStyles);
+    files.forEach(async file => {
+      if (path.extname(file) === '.css') {
+        const pathFile = path.join(pathStyles, file);
+        const contentFile = await fs.readFile(pathFile);
+        fs.appendFile(pathBundle, contentFile);
+      }
+    });
+  } catch (err) { stdout.write(`Well, it was possible to merge the files: ${err}`); }
+}
+
+createBundle();
